f81df7bac2174b342992b457a6fc965a06d50ad1
Nikos Maris <nickmeet@gmail.com>
2016-07-20 01:13:08 +0300

    ADD collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 cp -R media ../data/media
 cp db.sqlite3 ../data/db
 
+export COLLECT=True
+python manage.py collectstatic --noinput
+unset COLLECT
+
 python2.7 manage.py makemigrations
 python2.7 manage.py migrate
 python2.7 manage.py runserver 0.0.0.0:8085         # accessible to network, not only to localhost:8085
